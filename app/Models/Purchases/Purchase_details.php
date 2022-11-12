3682999313940fee8b391009cf13b9576160d8d8
Nasim Ahmed <nasimn01@github.com>
2022-11-13 00:23:21 +0600

    changes

--- app/Models/Purchases/Purchase_details.php
+++ app/Models/Purchases/Purchase_details.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Purchase extends Model
+class Purchase_details extends Model
 {
     use HasFactory,SoftDeletes;
 }
