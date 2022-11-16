c6c07267800b8cca8f822a33a054cdee264735fc
harunurR <harunurrashid97.ctg@gmail.com>
2022-11-16 11:35:18 +0600

    changes_saiful

--- app/Models/Customers/customer.php
+++ app/Models/Customers/customer.php
@@ -2,6 +2,10 @@
 
 namespace App\Models\Customers;
 
+use App\Models\Settings\Location\Country;
+use App\Models\Settings\Location\Division;
+use App\Models\Settings\Location\District;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,4 +13,14 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class customer extends Model
 {
     use HasFactory,SoftDeletes;
+
+    public function district(){
+        return $this->belongsTo(District::class);
+    }
+    public function division(){
+        return $this->belongsTo(Division::class);
+    }
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
 }
