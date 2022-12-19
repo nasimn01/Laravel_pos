1d84f47165b80c6a1ab920f1662c627bd5f8d1ea
shahidctg49 <shahiduli555@github.com>
2022-12-19 14:04:22 +0600

    changes nasim

--- resources/views/settings/adminusers/profile.blade.php
+++ resources/views/settings/adminusers/profile.blade.php
@@ -19,7 +19,14 @@
                 @if(Session::has('response'))
                     {!!Session::get('response')['message']!!}
                 @endif
-                <form class="form" method="post" enctype="multipart/form-data" action="{{route(currentUser().'.users.update',encryptor('encrypt',$users->id))}}">
+
+                @foreach ($errors->all() as $error)
+                <li class="text-danger">{{ $error }}</li>
+            @endforeach
+
+
+
+                <form class="form" method="post" enctype="multipart/form-data" action="{{route(currentUser().'.admin.update',encryptor('encrypt',$users->id))}}">
                     @csrf
                     @method('PATCH')
                     <input type="hidden" value="{{$users->role_id}}" name="role_id">
@@ -33,27 +40,33 @@
                         <label for="userEmail" class="col-sm-2 col-form-label"><b>{{__('Email')}}:</b></label>
                         <div class="col-sm-10">
                             <input type="email" name="userEmail" class="form-control" value="{{$users->email}}">
-                            
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="contactNumber" class="col-sm-2 col-form-label"><b>{{__('Contact')}}:</b></label>
                         <div class="col-sm-10">
                             <input type="text" id="contactNumber" name="contactNumber" class="form-control" value="{{$users->contact_no}}">
-                            
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="password" class="col-sm-2 col-form-label"><b>{{__('Password')}}:</b></label>
                         <div class="col-sm-10">
                             <input type="text" id="password" name="password" class="form-control">
-                        
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="language" class="col-sm-2 col-form-label"><b>{{__('Language:')}}</b></label>
+                        <div class="col-sm-10">
+                        <select class="form-control form-select" name="language">
+                            <option value="">Select Language</option>
+                            <option value="en" {{ old('language',$users->language)=="en"?"selected":""}}> English</option>
+                            <option value="bn" {{ old('language',$users->language)=="bn"?"selected":""}}> Bangla</option>
+                        </select>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="photo" class="col-sm-2 col-form-label"><b>{{__('Photo')}}:</b></label>
                         <div class="col-sm-10">
-                            
                             <input type="file" id="photo" name="image" class="form-control" onchange="pview(this)">
                             <img src="{{asset('images/users/'.company()['company_id'].'/'.$users->image)}}" id="photo_p" class="my-1" width="100px" alt="">
                         </div>
