5461f5283bbb55c6b6b18d9415c2d435540865a6
Nasim Ahmed <nashim.idb333@gmail.com>
2023-06-04 14:47:07 +0600

    add sales date picker

--- resources/views/purchase/create.blade.php
+++ resources/views/purchase/create.blade.php
@@ -85,7 +85,7 @@
                                     <label for="date" class="float-end"><h6>Date<span class="text-danger">*</span></h6></label>
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="text" id="datepicker" class="form-control" value="{{ old('purchase_date')}}" name="purchase_date" required>
+                                    <input type="text" id="datepicker" class="form-control" value="{{ old('purchase_date')}}" name="purchase_date" placeholder="dd/mm/yyyy" required>
                                 </div>
 
 
